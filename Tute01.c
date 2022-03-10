0fc1707dbd62ab44580fe72e7d678e599a55c5ef
it21255656 Karunananthan <>
2022-03-10 06:59:29 +0000

    Completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+
+  int mark1, mark2, sum;
+  float avg;
+
+  printf("Enter mark 1: ");
+  scanf("%d", &mark1);
+  printf("Enter mark 2: ");
+  scanf("%d", &mark2);
+  sum = mark1 + mark2;
+
+  avg = sum / 2.0;
+  printf("Average of two marks are: %f", avg);
   
   return 0;
 }
